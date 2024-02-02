68d4022c2c2af045640f0ff138f2258ee843557a
Nguyen Trung Kien <ntken651@gmail.com>
2024-02-02 21:46:06 +0700

    thêm realtime cho tính năng chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -3,18 +3,31 @@ const User = require("../../models/user.model");
 // [GET] /chat
 module.exports.index = async (req, res) => {
   const userId = res.locals.user.id;
+  const fullName = res.locals.user.fullName;
 
+  //Socket.IO
   _io.once("connection", (socket) => {
     socket.on("CLIENT_SEND_MESSAGE",async (content) => {
-        const chat = new Chat(
+        //Lưu vào database
+      const chat = new Chat(
           {
             user_id: userId,
           content: content
           }
         );
         await chat.save();
+
+
+        //Trả data chat về client
+        _io.emit("SERVER_RETURN_MESSAGE" ,{
+            userId : userId,
+            fullName : fullName,
+            content : content
+        } )
     });
   });
+
+  //Lấy ra data chat
   const chats = await Chat.find({
     deleted:false
   });
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,34 @@
 // CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector(".chat .inner-form");
-if(formSendData){
-    formSendData.addEventListener("submit", (e) =>{
-        e.preventDefault();
+if (formSendData) {
+  formSendData.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-        const content = e.target.elements.content.value;
-       if(content){
-        socket.emit("CLIENT_SEND_MESSAGE" , content);
-        e.target.elements.content.value = "";
-       }
-    });
+    const content = e.target.elements.content.value;
+    if (content) {
+      socket.emit("CLIENT_SEND_MESSAGE", content);
+      e.target.elements.content.value = "";
+    }
+  });
 }
+
+//SERVER_RETURN_MESSAGE
+
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  const myId = document.querySelector("[my-id]").getAttribute("my-id");
+  const body = document.querySelector(".chat .inner-body");
+
+  const div = document.createElement("div");
+  let htmlFullName= "";
+  if (myId == data.userId) {
+    div.classList.add("inner-outgoing");
+  } else {
+    div.classList.add("inner-incoming");
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+  }
+
+  div.innerHTML = `
+    ${htmlFullName}
+    <div class="inner-content">${data.content}</div>`;
+  body.appendChild(div);
+});
