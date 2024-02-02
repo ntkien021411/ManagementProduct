ebcf3d5284a0574b87187da08923a3b78d18fb90
Nguyen Trung Kien <ntken651@gmail.com>
2024-02-02 21:19:25 +0700

    tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,12 +1,32 @@
-
-
+const Chat = require("../../models/chat.model");
+const User = require("../../models/user.model");
 // [GET] /chat
-module.exports.index =  async (req, res) => {
-    _io.on('connection', (socket) => {
-        console.log('a user connected');
-        console.log(socket.id);
-      });
-  res.render("client/pages/chat/index",{
-    title:"Chat",
+module.exports.index = async (req, res) => {
+  const userId = res.locals.user.id;
+
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE",async (content) => {
+        const chat = new Chat(
+          {
+            user_id: userId,
+          content: content
+          }
+        );
+        await chat.save();
+    });
+  });
+  const chats = await Chat.find({
+    deleted:false
+  });
+  for (const chat of chats) {
+      const infoUser = await User.findOne({
+        _id : chat.user_id
+      }).select("fullName");
+      chat.infoUser = infoUser;
+  }
+  // console.log(chats);
+  res.render("client/pages/chat/index", {
+    title: "Chat",
+    chats : chats
   });
 };
